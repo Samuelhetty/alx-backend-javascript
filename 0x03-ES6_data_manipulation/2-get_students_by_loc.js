c3aea75508849537f3605144e3144f02c709b0dc
Henrietta <hetty8004@gmail.com>
2024-04-30 21:31:30 +0200

    data manipulation

---
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -0,0 +1,6 @@
+export default function getListStudentIds(students, city) {
+    if (studey5nts instanceof Array) {
+        return students.filter((student) => student.location === city);
+    }
+    return [];
+}
